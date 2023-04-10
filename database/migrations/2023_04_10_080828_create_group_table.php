a94bf9e60836a7155f50678638f6ae4fe4820bf7
anasbadrb <126650283+anasbadrb@users.noreply.github.com>
2023-04-10 11:26:05 +0300

    group

---
+++ database/migrations/2023_04_10_080828_create_group_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_ar');
+$table->string('name_en');
+$table->string('title_ar');
+$table->string('title_en');
+$table->longText('desc_ar');
+$table->longText('desc_en');
+$table->string('link');
+$table->string('images');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('group');
+    }
+};
