2a8b0784f0935bf0568ba3d965f022e879391651
Mousa0099 <kkssaamm0099@gmail.com>
2023-04-13 15:03:06 +0300

    ar and en contact

--- resources/lang/en/site.php
+++ resources/lang/en/site.php
@@ -12,13 +12,40 @@ return [
         'Connectus' => 'Connect us',
         'Subscribeus' => 'Subscribe us',
 
+        'Link' => ' Links',
+	'Subscribe' => ' Subscribe',
+	
+	 'copyright'=> 'All rights reserved by Jandala Business Solutions Management',
+        // end appPortal
+		
+		
+		
+		// homepages
+	'des_1' => 'WELCOME TO JANDALAH FOR BUSINESS MANAGEMENT',
+    'des_2' => 'With Jandalah Business Management, we created business solutions',
+    'des_3' => 'Business Solutions includes a team of experts in various fields to ensure the provision of integrated services to customers',
+	
+	'reviews' => 'Our customers opinions',
 
+	   // end homepages
+		
+		
+       // aboutUs
+    'us' => 'us ',
+    'about' => 'About',
+      //end  aboutUs
 
-
-
-
-
-
-
+         //contact
+         'with' => 'us',
+         'contact' => 'Contact',
+         'phone' => 'phone',
+         'location' => 'location ',
+         'inquiries' => 'inquiries',
+         'name' => 'name',
+         'email' => 'email',
+         'subject' => 'Title',
+         'message' => 'message',
+         'send' => 'send',
+         //end contact
 
 ];
